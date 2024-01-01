368923a5cc772ef730e40e22f6091b7c11afd8eb
unknown <thiernohabibbah176@gmail.com>
2024-01-01 18:08:15 +0100

    modification

--- src/App.js
+++ src/App.js
@@ -14,41 +14,9 @@ import Login from './Authentification/Login';
 
 
 function App() {
-  const [id, setId] = useState("");
-  const [nom, setNom] = useState("");
-  const [prenom, setPrenom] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-
-
-  // const handleAddData = () => {
-  //   // Récupère les données existantes depuis localStorage
-  //   const existingData = JSON.parse(localStorage.getItem("mesDonnees")) || [];
-
-  //   // Crée un nouvel objet pour la nouvelle donnée
-  //   const newData = { id, nom,prenom,email,password};
-
-  //   // Ajoute la nouvelle donnée aux données existantes
-  //   const updatedData = [...existingData, newData];
-
-  //   // Met à jour localStorage avec les nouvelles données
-  //   localStorage.setItem("mesDonnees", JSON.stringify(updatedData));
-
-  //   console.log(existingData);
-
-  //   // Réinitialise les champs après l'ajout
-  //   setId("");
-  //   setNom("");
-  //   setPrenom("");
-  //   setEmail("");
-  //   setPassword("");
-  // };
-
   return (
     <Router>
       {/* <Menu></Menu> */}
-      
       <Routes>
           <Route path="" element={<Home />} />
           <Route path="/login" element={<Login />} />
